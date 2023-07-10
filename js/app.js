d7a8087e4220d5a1717bfecbb3892947d5d9cd12
Artem Tykhonenko <128739984+Ferragone@users.noreply.github.com>
2023-07-10 15:46:42 +0300

    Src

--- js/app.js
+++ js/app.js
@@ -2,4 +2,14 @@ const sliderMain = new Swiper(".slider_main", {
   freeMode: true,
   centeredSlider: true,
   mousewheel: true,
+  breakpoint: {
+    0: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    680: {
+      slidesPerView: 3.5,
+      spaceBetween: 60,
+    },
+  },
 });
